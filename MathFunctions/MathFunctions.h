4e346262c98dd84eeecd981dcef749502b3990aa
xiaotaoqi-yy <yanyu860625@gmail.com>
2018-05-15 10:45:14 +0800

    First commit.

---
+++ MathFunctions/MathFunctions.h
@@ -0,0 +1,11 @@
+//
+// Created by yy on 18-5-14.
+//
+
+#ifndef TUTORIAL_MATHFUNCTIONS_H
+#define TUTORIAL_MATHFUNCTIONS_H
+
+double mysqrt(double x);
+
+#endif //TUTORIAL_MATHFUNCTIONS_H
+
---
+++ MathFunctions/mysqrt.cxx
@@ -0,0 +1,9 @@
+//
+// Created by yy on 18-5-14.
+//
+#include "MathFunctions.h"
+#include <math.h>
+
+double mysqrt(double x){
+  return pow(x,0.5);
+}
