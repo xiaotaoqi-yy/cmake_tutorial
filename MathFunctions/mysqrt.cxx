5686692239eeca55e3a405bfa94e986b3707b3b1
xiaotaoqi-yy <yanyu860625@gmail.com>
2018-05-17 16:21:19 +0800

    Finish the cmake tutorial step 6.

--- MathFunctions/mysqrt.cxx
+++ MathFunctions/mysqrt.cxx
@@ -3,7 +3,15 @@
 //
 #include "MathFunctions.h"
 #include <math.h>
+#include "TutorialConfig.h"
 
 double mysqrt(double x){
-  return pow(x,0.5);
+  double result;
+// if we have both log and exp then use them
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  result = exp(log(x)*0.5);
+#else
+  result = pow(x,0.5);
+#endif
+  return result;
 }
